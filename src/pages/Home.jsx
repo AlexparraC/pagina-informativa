16b445e6d5c4fbbed7c8415501d79d7caca98e82
Jeison Stiven Lopez Castro <jeisonlopez2599@gmail.com>
2025-06-18 21:45:49 -0500

    Se modifico la pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Cards from "../components/cards";
 function Home() {
     return (
         <>
-            <h1 class=" text-[#03658C] text-4xl text-center">Bienvenidos</h1>
+            <h1 class=" text-[#03658C] font-extrabold text-4xl text-center">Bienvenidos</h1>
             <Cards />
         </>
     )
